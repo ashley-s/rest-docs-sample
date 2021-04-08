df89a6dc877a615873ac8f001a1776a599a30941
MCB\ashsho <ashley.shookhye@mcb.mu>
2021-04-08 20:53:01 +0400

    rest docs testing

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+    @Configuration()
+    static class RestDocsConfiguration {
+        @Bean
+        public RestDocsMockMvcConfigurationCustomizer restDocsMockMvcConfigurationCustomizer() {
+            return configurer -> configurer.operationPreprocessors().withResponseDefaults(Preprocessors.prettyPrint());
+        }
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHello() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/payroll/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello world"));
+    }
+
+}
